f7d162a4e1cf500b4525245db2a8cb5041b6aef3
chrismpettyjohn <chrismpettyjohn@gmail.com>
2024-11-11 10:32:01 -0500

    feat: corp list

--- src/main/java/com/us/archangel/feature/corp/packets/outgoing/CorpListComposer.java
+++ src/main/java/com/us/archangel/feature/corp/packets/outgoing/CorpListComposer.java
@@ -1,10 +1,14 @@
 package com.us.archangel.feature.corp.packets.outgoing;
 
+import com.eu.habbo.Emulator;
+import com.eu.habbo.habbohotel.users.Habbo;
 import com.eu.habbo.messages.ServerMessage;
 import com.eu.habbo.messages.outgoing.MessageComposer;
 import com.eu.habbo.messages.outgoing.Outgoing;
 import com.us.archangel.corp.model.CorpModel;
 import com.us.archangel.corp.service.CorpService;
+import com.us.archangel.player.model.PlayerModel;
+import com.us.archangel.player.service.PlayerService;
 import lombok.AllArgsConstructor;
 import java.util.List;
 
@@ -20,9 +24,18 @@ public class CorpListComposer extends MessageComposer {
 
         // Append the sorted positions to the response
         for (CorpModel corp : corps) {
+            Habbo owner = Emulator.getGameEnvironment().getHabboManager().getHabbo(corp.getUserId());
+            List<PlayerModel> employees =PlayerService.getInstance().getByCorpId(corp.getId());
+
             this.response.appendString(
                     corp.getId()
                             + ";" + corp.getDisplayName()
+                            + ";" + corp.getBadge()
+                            + ";" + corp.getIndustry()
+                            + ";" + corp.getSector()
+                            + ";" + corp.getUserId()
+                            + ";" + owner.getHabboInfo().getUsername()
+                            + ";" + employees.size()
             );
         }
 
