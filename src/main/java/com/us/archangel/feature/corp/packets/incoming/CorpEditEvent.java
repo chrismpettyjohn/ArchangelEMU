911d8388af3ca2d826ea726e780f9283639f3739
chrismpettyjohn <chrismpettyjohn@gmail.com>
2024-11-11 23:44:53 -0500

    feat: corp editing e2e

--- src/main/java/com/us/archangel/feature/corp/packets/incoming/CorpEditEvent.java
+++ src/main/java/com/us/archangel/feature/corp/packets/incoming/CorpEditEvent.java
@@ -2,8 +2,10 @@ package com.us.archangel.feature.corp.packets.incoming;
 
 import com.eu.habbo.habbohotel.permissions.Permission;
 import com.eu.habbo.messages.incoming.MessageHandler;
+import com.us.archangel.corp.entity.CorpEntity;
 import com.us.archangel.corp.enums.CorpIndustry;
 import com.us.archangel.corp.enums.CorpSector;
+import com.us.archangel.corp.mapper.CorpMapper;
 import com.us.archangel.corp.model.CorpModel;
 import com.us.archangel.corp.service.CorpService;
 import com.us.archangel.feature.corp.packets.outgoing.CorpInfoComposer;
@@ -23,14 +25,15 @@ public class CorpEditEvent extends MessageHandler {
             return;
         }
 
-        corp.setDisplayName(this.packet.readString());
-        corp.setDescription(this.packet.readString());
-        corp.setUserId(this.packet.readInt());
-        corp.setRoomId(this.packet.readInt());
-        corp.setSector(CorpSector.fromString(this.packet.readString()));
-        corp.setIndustry(CorpIndustry.fromString(this.packet.readString()));
+        CorpEntity corpEntity = CorpMapper.toEntity(corp);
+        corpEntity.setDisplayName(this.packet.readString());
+        corpEntity.setDescription(this.packet.readString());
+        corpEntity.setUserId(this.packet.readInt());
+        corpEntity.setRoomId(this.packet.readInt());
+        corpEntity.setSector(CorpSector.fromString(this.packet.readString()));
+        corpEntity.setIndustry(CorpIndustry.fromString(this.packet.readString()));
 
-        CorpService.getInstance().update(corp.getId(), corp);
+        CorpService.getInstance().update(corp.getId(), CorpMapper.toModel(corpEntity));
 
         this.client.sendResponse(new CorpInfoComposer(corp.getId()));
     }
