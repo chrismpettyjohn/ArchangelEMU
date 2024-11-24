e0b3119744248b20d8e8e96d366d1f9725cb6540
chrismpettyjohn <chrismpettyjohn@gmail.com>
2024-11-23 20:08:43 -0500

    feat: configure hospital health per cycle

--- src/main/java/com/us/archangel/feature/paramedic/actions/HospitalRecoveryAction.java
+++ src/main/java/com/us/archangel/feature/paramedic/actions/HospitalRecoveryAction.java
@@ -3,43 +3,39 @@ package com.us.archangel.feature.paramedic.actions;
 import com.eu.habbo.Emulator;
 import com.eu.habbo.habbohotel.users.Habbo;
 import com.us.archangel.feature.hospital.interactions.InteractionHospitalBed;
+import com.us.archangel.feature.player.packets.outgoing.UserRoleplayStatsChangeComposer;
 import com.us.archangel.room.enums.RoomType;
+import com.us.nova.core.NotificationHelper;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class HospitalRecoveryAction implements Runnable {
 
-    public static final int HEALTH_PER_CYCLE = 5;
-
     private final Habbo habbo;
-    private boolean isHealing = true; // Ensures no further cycles run after healing completes
-
     @Override
     public void run() {
 
         if (this.habbo.getRoomUnit().getRoom() == null) {
-            this.isHealing = false;
             return;
         }
 
         if (!this.habbo.getRoomUnit().getRoom().getRoomInfo().getTags().contains(RoomType.HOSPITAL)) {
             this.habbo.shout(Emulator.getTexts().getValue("roleplay.hospital.cancel_recovery"));
-            this.isHealing = false;
             return;
         }
 
         if (this.habbo.getRoomUnit().getCurrentItem().getBaseItem().getInteractionType().getType() != InteractionHospitalBed.class) {            this.habbo.shout(Emulator.getTexts().getValue("roleplay.hospital.cancel_recovery"));
-            this.isHealing = false;
             return;
         }
 
         if (this.habbo.getPlayer().getHealthNow() == this.habbo.getPlayer().getHealthMax()) {
-            this.isHealing = false;
             this.habbo.shout(Emulator.getTexts().getValue("roleplay.hospital.finish_recovery"));
             return;
         }
 
-        this.habbo.getPlayer().addHealth(HEALTH_PER_CYCLE);
+        this.habbo.getPlayer().addHealth(Emulator.getConfig().getInt("roleplay.hospital.health_per_cycle"));
+
+        NotificationHelper.sendRoom(this.habbo.getRoomUnit().getRoom().getRoomInfo().getId(), new UserRoleplayStatsChangeComposer(this.habbo));
 
         this.habbo.shout(Emulator.getTexts()
                 .getValue("roleplay.hospital.progress_recovery")
