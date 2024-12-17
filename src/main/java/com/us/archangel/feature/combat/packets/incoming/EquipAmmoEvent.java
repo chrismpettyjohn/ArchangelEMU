a3bb992fd31ea9947cb210e55baa4c88a7439bb6
chrismpettyjohn <chrismpettyjohn@gmail.com>
2024-12-17 15:15:31 -0500

    fix: change ammo

--- src/main/java/com/us/archangel/feature/combat/packets/incoming/EquipAmmoEvent.java
+++ src/main/java/com/us/archangel/feature/combat/packets/incoming/EquipAmmoEvent.java
@@ -1,5 +1,6 @@
 package com.us.archangel.feature.combat.packets.incoming;
 
+import com.eu.habbo.Emulator;
 import com.eu.habbo.messages.incoming.MessageHandler;
 import com.us.archangel.feature.player.packets.outgoing.UserRoleplayStatsChangeComposer;
 import com.us.archangel.player.model.PlayerAmmoModel;
@@ -26,6 +27,11 @@ public class EquipAmmoEvent extends MessageHandler {
         }
 
         playerWeapon.setAmmoId(ammoId);
+
+        this.client.getHabbo().shout(Emulator.getTexts()
+                .getValue("roleplay.equip_ammo.success")
+                .replace(":ammo", playerAmmo.getAmmo().getDisplayName())
+        );
 
         this.client.sendResponse(new UserRoleplayStatsChangeComposer(this.client.getHabbo()));
     }
