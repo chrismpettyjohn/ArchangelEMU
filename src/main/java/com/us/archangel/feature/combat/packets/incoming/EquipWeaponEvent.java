d00bfb93dcf311e1d3b59953482ec8a7fb57e78c
chrismpettyjohn <chrismpettyjohn@gmail.com>
2024-12-17 22:22:01 -0500

    fix: handitem not showing when changing to tool after gun

--- src/main/java/com/us/archangel/feature/combat/packets/incoming/EquipWeaponEvent.java
+++ src/main/java/com/us/archangel/feature/combat/packets/incoming/EquipWeaponEvent.java
@@ -16,14 +16,13 @@ public class EquipWeaponEvent extends MessageHandler {
     @Override
     public void handle() {
         int playerWeaponId = this.packet.readInt();
+        
+        this.client.getHabbo().getRoomUnit().giveEffect(0, -1);
+        Emulator.getThreading().run(new RoomUnitGiveHanditem(this.client.getHabbo().getRoomUnit(), this.client.getHabbo().getRoomUnit().getRoom(), 0));
 
         if (playerWeaponId == 0) {
             PlayerWeaponModel prevEquippedWeapon = this.client.getHabbo().getInventory().getWeaponsComponent().getEquippedWeapon();
             this.client.getHabbo().getInventory().getWeaponsComponent().setEquippedWeapon(null);
-            this.client.getHabbo().getRoomUnit().giveEffect(0, -1);
-            if (this.client.getHabbo().getRoomUnit().getHandItem() > 0) {
-                Emulator.getThreading().run(new RoomUnitGiveHanditem(this.client.getHabbo().getRoomUnit(), this.client.getHabbo().getRoomUnit().getRoom(), 0));
-            }
             this.client.getHabbo().getRoomUnit().getRoom().sendComposer(new UserRoleplayStatsChangeComposer(this.client.getHabbo()).compose());
             this.client.getHabbo().shout(prevEquippedWeapon.getWeapon().getUnequipMessage().replace(":displayName", prevEquippedWeapon.getWeapon().getDisplayName()));
             return;
