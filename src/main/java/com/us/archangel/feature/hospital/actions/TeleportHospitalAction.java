5f33086a4a3a580669590d264d00bee4197ef47a
chrismpettyjohn <chrismpettyjohn@gmail.com>
2025-01-11 14:33:42 -0500

    fix: lay on hospital bed on tele

--- src/main/java/com/us/archangel/feature/hospital/actions/TeleportHospitalAction.java
+++ src/main/java/com/us/archangel/feature/hospital/actions/TeleportHospitalAction.java
@@ -4,6 +4,7 @@ import com.eu.habbo.Emulator;
 import com.eu.habbo.habbohotel.commands.list.LayCommand;
 import com.eu.habbo.habbohotel.rooms.Room;
 import com.eu.habbo.habbohotel.rooms.RoomTile;
+import com.eu.habbo.habbohotel.rooms.entities.RoomRotation;
 import com.eu.habbo.habbohotel.rooms.items.entities.RoomItem;
 import com.eu.habbo.habbohotel.users.Habbo;
 import com.us.archangel.feature.hospital.interactions.InteractionHospitalBed;
@@ -18,16 +19,13 @@ import java.util.List;
 public class TeleportHospitalAction implements Runnable {
 
     private final Habbo habbo;
+    private Room hospitalRoom = Emulator.getGameEnvironment().getRoomManager().getActiveRooms().stream().filter(Room::isHospital).findFirst().orElse(null);
 
     @Override
     public void run() {
         if (this.habbo.getRoomUnit() == null) {
             return;
         }
-
-//        if (this.habbo.getRoomUnit().getRoom().isHospital()) {
-//            return;
-//        }
 
         if (this.habbo.getPlayer().getEscortingPlayerId() != null) {
             this.habbo.getPlayer().setCurrentAction(PlayerAction.None);
@@ -39,28 +37,44 @@ public class TeleportHospitalAction implements Runnable {
 
         int deadTeleportDelay = Emulator.getConfig().getInt("roleplay.dead.delay", 10000);
 
-        new LayCommand().handle(this.habbo.getClient(), new String[0]);
-
         this.habbo.shout(Emulator.getTexts().getValue("roleplay.dead.teleporting_to_hospital_delay").replace(":seconds", String.valueOf(deadTeleportDelay / 1000)));
 
-
-        Room hospitalRoom = Emulator.getGameEnvironment().getRoomManager().getActiveRooms().stream().filter(room -> room.isHospital()).findFirst().orElse(null);
-
-        if (hospitalRoom == null) {
+        if (this.hospitalRoom == null) {
             throw new RuntimeException("no hospitals found");
         }
 
-        habbo.goToRoom(hospitalRoom.getRoomInfo().getId(), () -> {
-            Collection<RoomItem> hospitalBedItems = hospitalRoom.getRoomItemManager().getItemsOfType(InteractionHospitalBed.class);
-            for (RoomItem hospitalBedItem : hospitalBedItems) {
-                List<RoomTile> hospitalBedRoomTiles = hospitalBedItem.getOccupyingTiles(hospitalRoom.getLayout());
-                RoomTile firstAvailableHospitalBedTile = hospitalBedRoomTiles.isEmpty() ? null : hospitalBedRoomTiles.get(0);
-                if (firstAvailableHospitalBedTile == null) {
-                    return;
-                }
-                habbo.getRoomUnit().setLocation(firstAvailableHospitalBedTile);
-                break;
-            }
-        });
+        if (this.habbo.getRoomUnit().getRoom().isHospital()) {
+            this.hospitalRoom = this.habbo.getRoomUnit().getRoom();
+            this.teleportAndLayOnHospitalBed();
+            return;
+        }
+
+        habbo.goToRoom(this.hospitalRoom.getRoomInfo().getId(), this::teleportAndLayOnHospitalBed);
     }
+
+    private void teleportAndLayOnHospitalBed() {
+        Collection<RoomItem> hospitalBedItems = this.hospitalRoom.getRoomItemManager().getItemsOfType(InteractionHospitalBed.class);
+        for (RoomItem hospitalBedItem : hospitalBedItems) {
+            List<RoomTile> hospitalBedRoomTiles = hospitalBedItem.getOccupyingTiles(this.hospitalRoom.getLayout());
+            RoomTile firstAvailableHospitalBedTile = hospitalBedRoomTiles.isEmpty() ? null : hospitalBedRoomTiles.get(0);
+
+            if (firstAvailableHospitalBedTile == null || !canLayOnBed(hospitalBedItem, firstAvailableHospitalBedTile)) {
+                continue;
+            }
+
+            RoomTile pillow = switch (hospitalBedItem.getRotation()) {
+                case 0, 4 -> this.hospitalRoom.getLayout().getTile(hospitalBedItem.getCurrentPosition().getX(), hospitalBedItem.getCurrentPosition().getY());
+                case 2, 8 -> this.hospitalRoom.getLayout().getTile(hospitalBedItem.getCurrentPosition().getX(), hospitalBedItem.getCurrentPosition().getY());
+                default -> firstAvailableHospitalBedTile;
+            };
+
+            habbo.getRoomUnit().setLocation(pillow);
+            break;
+        }
+    }
+
+    private boolean canLayOnBed(RoomItem bed, RoomTile tile) {
+        return bed != null && bed.getBaseItem().allowLay() && this.hospitalRoom.canLayAt(tile);
+    }
+
 }
